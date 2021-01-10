9488e04c02d359fd74e49c3a952d8fac51446f93
kacarmichael <kacarmichael72@gmail.com>
2021-01-09 18:17:07 -0600

    Added GPU Test

---
+++ mnist_cnn_gpu.py
@@ -0,0 +1,10 @@
+import numpy as np
+import tensorflow as tf
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPool2D
+
+from tensorflow.python.client import device_lib
+from keras import backend as K
